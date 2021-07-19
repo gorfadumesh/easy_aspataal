4c287b8472a41d118375fa0be8777a85d11a88e8
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-07-19 22:10:34 +0530

    ad

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function NavbarHeader() {
     return (
         <div>
             <>
-                <Navbar style={{ backgroundColor: 'white' }} expand="xxl " className="nav-one">
+                <Navbar bg="white" expand="xxl " className="nav-one">
                     <Container>
 
 

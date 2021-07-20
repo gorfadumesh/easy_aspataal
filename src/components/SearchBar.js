2e8fdfef4e49cd6e8ada86cbf53d41919f422d9d
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-07-20 11:56:28 +0530

    added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,39 +4,28 @@ import { Container, FormControl, Button, Row, Col } from "react-bootstrap"
 export default function SearchBar() {
     return (
         <div className="search-bar">
-            {/* <h1>Search Bar</h1> */}
-            <Container className="justify-content-md-center">
-                {/* <Row className="justify-content-md-center"> */}
 
-                <div className="search-bar-inner">
 
-                    {/* <FormControl
-                        type="search"
-                        placeholder="Search"
-                        // className="mr-2"
-                        aria-label="Search"
-                    />
-                    <Button className="">Search</Button> */}
-                    {/* <input></input>
-                    <button>Search</button> */}
-                    {/* </Row> */}
-                    {/* <Row className="justify-content-md-center"> */}
-                    <div className="primary-font">
-                        <h2>How Can We Help You ?</h2>
-                        <input className="search-input"></input>
-                        <button className="button" style={{ backgroundColor: "#FB8B4C" }}>Search</button>
+            <div className="search-bar-inner">
+
+
+                <div className="primary-font">
+                    <h1 >How Can We Help You ?</h1>
+                    <div className="search-bar2">
+                        <input className="search-input" placeholder="Diagnosis, Type of Treatment" ></input>
+                        <button className="search-button" style={{ backgroundColor: "#FB8B4C" }}>Search</button>
                     </div>
+                </div>
 
-                    <div className="recent-search" >
-                        <h6 className="primary-font">Recent Searches :</h6>  &emsp; <a className="link-font" href="#home"><h6>Lung Canecer ,</h6></a>  &emsp; <a className="link-font" href="#home"><h6>Angioscopy ,</h6></a>  &emsp; <a className="link-font" href="#home"><h6>Bypass Surgery</h6></a>
-                    </div>
-
-
+                <div className="recent-search" >
+                    <h6 style={{ color: "white" }} >Recent Searches :</h6>  &emsp; <a className="link-font" href="#home"><h6>Lung Canecer ,</h6></a>  &emsp; <a className="link-font" href="#home"><h6>Angioscopy ,</h6></a>  &emsp; <a className="link-font" href="#home"><h6>Bypass Surgery</h6></a>
                 </div>
 
 
-                {/* </Row> */}
-            </Container>
+            </div>
+
+
+
         </div >
     )
 }

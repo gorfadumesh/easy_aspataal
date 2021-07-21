b6d9bcd8e1d11658cdc72df12c9949aa68e07356
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-07-21 20:20:03 +0530

    UI fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -117,7 +117,7 @@ export default function NavbarHeader() {
             </>
             <div className="carousel">
 
-                <Carousel fade >
+                <Carousel fade nextLabel="" prevLabel="">
                     <Carousel.Item >
                         <div className="overlay">
                             <img

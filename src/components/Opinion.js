bcb6cadd3c850bf869c78206fe5b719c729518a7
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-07-28 19:47:20 +0530

    UI Changes added

--- src/components/Opinion.js
+++ src/components/Opinion.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import portrait from "../assets/Speciality Page Asset/portrait.png"
 
 export default function Opinion() {
     return (
         <div className="opinion">
             <p className="opinion-heading"> Get <span style={{ color: "#FB8B4C" }}>Second</span>  Opinion</p>
             <div className="opinion-body">
-                <input className="opinion-input" style={{ color: "grey" }} placeholder=" Full Name" />
-                <input className="opinion-input" style={{ color: "grey" }} placeholder=" Mobile" />
-                <button className="opnion-button" style={{ backgroundColor: "#FB8B4C" }}>Submit</button>
+                <div>
+                    <img src={portrait}></img>
+                </div>
+                <div className="opinion-form">
+                    <input className="opinion-input" style={{ color: "grey" }} placeholder=" Full Name" />
+                    <input className="opinion-input" style={{ color: "grey" }} placeholder=" Mobile" />
+                    <button className="opnion-button" style={{ backgroundColor: "#FB8B4C" }}>Submit</button>
+                </div>
+
             </div>
         </div >
     )

f7a93464aee532da11be8ba9b2c35decc4dd341c
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-07-30 11:32:39 +0530

    ui changes

--- src/components/Speciality.js
+++ src/components/Speciality.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Col, Row, Container } from "react-bootstrap"
+import { div } from "react-bootstrap"
 
 
 export default function Speciality() {
     return (
         <div className="speciality">
             <h1 className="about-heading"> Our Specialities</h1>
-            <Container>
-                <Row>
-                    <Col>
+            <div className="speciality-container">
+                <div className="speciality-list">
+                    <div >
                         <a  >  <h3 className="spec-font" style={{ color: "#0C065E" }}>Cardiology </h3> </a>
                         <a href="./speciality" >  <h5>Angiography(Day Care)</h5> </a>
                         <a  >  <h5>Angiography With Angioplasty</h5> </a>
@@ -17,8 +17,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
 
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -26,8 +26,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -35,8 +35,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -44,8 +44,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -53,8 +53,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -62,11 +62,11 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
+                    </div>
 
-                </Row>
-                <Row>
-                    <Col>
+                </div>
+                <div className="speciality-list">
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -74,8 +74,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -83,8 +83,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -92,8 +92,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -101,8 +101,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -110,8 +110,8 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
-                    <Col>
+                    </div>
+                    <div>
                         <a  >  <h3 className="spec-font">Speciality 1</h3> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
@@ -119,11 +119,11 @@ export default function Speciality() {
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
                         <a  >  <h5>Hyteroscopy</h5> </a>
-                    </Col>
+                    </div>
 
-                </Row>
+                </div>
 
-            </Container>
+            </div>
         </div>
     )
 }

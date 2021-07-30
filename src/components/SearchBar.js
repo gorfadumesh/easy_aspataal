353825379a8f3b5b0c458e49da492575566603a3
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-07-30 10:43:16 +0530

    Ui changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,15 +11,18 @@ export default function SearchBar() {
 
                 <div className="primary-font">
                     <h1 >How Can We Help You ?</h1>
-                    <div className="search-bar2">
-                        <input className="search-input" placeholder="Diagnosis, Type of Treatment" ></input>
-                        <button className="search-button" style={{ backgroundColor: "#FB8B4C" }}>Search</button>
+                    <div>
+                        <div className="search-bar2">
+                            <input className="search-input" placeholder="Diagnosis, Type of Treatment" ></input>
+                            <button className="search-button" style={{ backgroundColor: "#FB8B4C" }}>Search</button>
+                        </div>
+                        <div className="search-bar2" >
+                            <span style={{ color: "white" }} >Recent Searches :</span>  &emsp; <a className="link-font" href="#home">Lung Cancer,</a>&emsp; <a className="link-font" href="#home">Angioscopy,</a>&emsp; <a className="link-font" href="#home">Bypass Surgery</a>
+                        </div>
                     </div>
                 </div>
 
-                <div className="recent-search" >
-                    <span style={{ color: "white" }} >Recent Searches :</span>  &emsp; <a className="link-font" href="#home">Lung Cancer </a>  &emsp; <a className="link-font" href="#home">Angioscopy </a>  &emsp; <a className="link-font" href="#home">Bypass Surgery</a>
-                </div>
+
 
 
             </div>

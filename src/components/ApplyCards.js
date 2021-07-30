571e46e21d52124b173eb936d6f789625a0111cd
Vijay Gorfad <gorfadvijay1234@gmail.com>
2021-07-30 11:04:51 +0530

    ui changes

--- src/components/ApplyCards.js
+++ src/components/ApplyCards.js
@@ -14,7 +14,7 @@ export default function ApplyCards() {
                     <h1 className="apl-card-heading">Plan a Surgery</h1>
                     <div className="apl-card-text">
                         <h4 className="apl-card-heading2"  >Looking for a treatment ?</h4>
-                        <h4 className="apl-card-p">Find your surgery price and plan your treatment inyour nearest Hospital</h4>
+                        <h4 className="apl-card-p">Find your surgery price and plan your treatment in your nearest Hospital</h4>
 
                     </div>
                     <div>  <button className="button" style={{ backgroundColor: "#FB8B4C" }}>Apply Now</button></div>
@@ -26,7 +26,7 @@ export default function ApplyCards() {
                     <h1 className="apl-card-heading">Finance Your Surgery</h1>
                     <div className="apl-card-text">
                         <h4 className="apl-card-heading2"  >Worried About Medical Expenses</h4>
-                        <h4 className="apl-card-p">Find your surgery price<br />  and plan your treatment in<br /> your nearest Hospital</h4>
+                        <h4 className="apl-card-p">Avail 0% EMI instant medical <br /> loan for your surgery </h4>
 
                     </div>
                     <div> <button className="button" style={{ backgroundColor: "#FB8B4C" }}>Apply Now</button></div>
